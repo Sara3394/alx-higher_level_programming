dc42292b9f8951f66fca8772227f19666c58bd44
Sara3394 <hailu.serafina@yahoo.com>
2022-10-20 08:35:05 -0700

    commit

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """
-prints
-First name and Last name
+This function prints my name
+Returns:
+My name
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """ prints last and first if they are string """
-    if type(first_name) is not str or first_name == "":
-        raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
-        raise TypeError("last_name must be a string")
-    else:
-        print(f"My name is {first_name} {last_name}")
+    """prints a name to stdout
+    Returns name
+    """
+    try:
+        print(first_name + " " + last_name)
+    except TypeError:
+        if not (isinstance(first_name, str)):
+            raise TypeError("first_name must be a string")
+        else:
+            raise TypeError("last_name must be a string")
