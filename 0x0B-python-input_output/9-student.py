221cb8c38fec40c82053b2cf4b8b03685e2b773a
Sara3394 <hailu.serafina@yahoo.com>
2022-11-18 03:31:25 -0800

    fix code

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,29 +1,21 @@
-i#!/usr/bin/python3
-"""
-Module 9-student
-Contains class Student
-"""
+#!/usr/bin/python3
+"""Defines a class Student."""
 
 
-class Student():
-    """
-    Public Attributes:
-        first_name
-        last_name
-        age
-    Public Methods:
-        to_json
-    """
+class Student:
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes student
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """
-        Returns dictionary description with simple data structure
-        """
+        """Get a dictionary representation of the Student."""
         return self.__dict__
