d7116d67a6812d151db1514af0e8e4ff5b0f0422
Sara3394 <hailu.serafina@yahoo.com>
2023-01-25 22:30:36 +0300

    all

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" 100-github_commits.py """
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    r = requests.get(url)
+    try:
+        for i in range(10):
+            print("{}: {}".format(r.json()[i].get("sha"),
+                                  r.json()[i].get("commit").get("author")
+                                  .get("name")))
+    except IndexError:
+        pass
+
