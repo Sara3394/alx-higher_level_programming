6366d67312144135641e2401fd4ada59ab3cc412
Sara3394 <hailu.serafina@yahoo.com>
2023-01-25 21:06:41 +0300

    update file

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,30 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-    if (error) {
-        console.log(error);
+const url = 'http://swapi.co/api/films/';
+let id = parseInt(process.argv[2], 10);
+let characters = [];
+
+request(url, function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const results = resp.results;
+    if (id < 4) {
+      id += 3;
     } else {
-        const characters = JSON.parse(body).characters;
-        for (const character of characters) {
-        request(character, function (error, response, body) {
-            if (error) {
-            console.log(error);
-            } else {
-            console.log(JSON.parse(body).name);
-            }
-        });
-        }
+      id -= 3;
     }
-    });
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].episode_id === id) {
+        characters = results[i].characters;
+        break;
+      }
+    }
+    for (let j = 0; j < characters.length; j++) {
+      request(characters[j], function (err, response, body) {
+        if (err == null) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
