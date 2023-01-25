aa1ecf4d06430d256b0ca847a9b6bd70d87fb647
Sara3394 <hailu.serafina@yahoo.com>
2023-01-25 21:40:39 +0300

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" 6. Find a peak """
+
+def find_peak(list_of_integers):
+    """ find_peak """
+    if list_of_integers == []:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
+    if list_of_integers[0] > list_of_integers[1]:
+        return list_of_integers[0]
+    if list_of_integers[-1] > list_of_integers[-2]:
+        return list_of_integers[-1]
+    for i in range(1, len(list_of_integers) - 1):
+        if list_of_integers[i] > list_of_integers[i - 1] and \
+           list_of_integers[i] > list_of_integers[i + 1]:
+            return list_of_integers[i]
